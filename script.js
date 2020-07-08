237f0f121de168f8ff2bd07048f388a66f8ddf64
Jay Kim <djayshine87@hotmail.com>
2020-07-08 23:00:34 +1000

    word function working. just need timer

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
+//x is from data file
+
 const y = x.split(' ').join(',').split('.').join(',').split(',')
-console.log(y)
-console.log(y[Math.floor(Math.random()*1290)])
-console.log(y[Math.floor(Math.random()*1290)])
 
-const word = `${y[Math.floor(Math.random()*1290)]} ${y[Math.floor(Math.random()*1290)]} ${y[Math.floor(Math.random()*1290)]}`
+const z = y.filter(a=>{
+    if(a != ''){
+        return true
+    }
+})
 
-document.getElementById('the_word').innerHTML = word
+let score = 0;
+let time = 10;
+let word = ''
+document.getElementById('score').innerHTML = score
+
+const word_gen = ()=>{
+    const randomWord = `${z[Math.floor(Math.random()*1290)]} ${z[Math.floor(Math.random()*1290)]} ${z[Math.floor(Math.random()*1290)]}`
+    word = randomWord
+    document.getElementById('the_word').innerHTML = randomWord
+    console.log(randomWord)
+}
+
+document.getElementById('user_input').addEventListener('input',(e)=>{
+    if(e.target.value === word){
+        score += 1;
+        document.getElementById('score').innerHTML = score;
+        word_gen();
+        e.target.value = '';
+    }
+})
+
+word_gen();
